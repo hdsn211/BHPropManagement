2565bc8866cb96608f9973e1f0237a06cd069e38
Hudson <hudson.gacer@jmc.edu.ph>
2026-04-25 15:22:13 +0800

    Cleaning again

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 LOGIN_URL = 'login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
